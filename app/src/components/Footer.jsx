34baf55928a367ee0d7c0ee24546fea27e4fc61f
maite <terealbenca@gmail.com>
2025-06-19 20:05:17 +0200

    change portfolio url

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer({ onLogout, isHome, onPrivacyClick }) {
     return (
         <footer className='h-[42px] sm:h-[46px] lg:h-[50px] w-full bg-cyan-600 p-2 px-1 fixed bottom-0 flex justify-between items-center left-0'>
             <div className='flex space-x-2 px-2 items-center h-full'>
-                <Links href='https://portfolio-alallunas-projects.vercel.app/' imgSrc='../images/portfolio.png' alt='portfolio alalluna' />
+                <Links href='https://portfolio-alallunas.vercel.app/' imgSrc='../images/portfolio.png' alt='portfolio alalluna' />
                 <Links href='https://github.com/alalluna' imgSrc='../images/github.png' alt='alalluna on github' />
                 <Links href='https://behance.net/alalluna' imgSrc='../images/behance.png' alt='alalluna on behance' />
                 <Links href='https://www.linkedin.com/in/alalluna/' imgSrc='../images/linkedin.png' alt='alalluna on linkedin' />
